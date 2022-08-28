d3db0496c7f0a4f77559872ad57641475b641869
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-08-28 11:35:56 -0400

    Update plot location and map section

--- Moraland_Frontend/static/js/logic.js
+++ Moraland_Frontend/static/js/logic.js
@@ -14,11 +14,11 @@ const plotCtx = plotCanvas.getContext("2d");
 const worldImage = new Image();
 
 // - State
-const mapView = {mapOffsetX: -1 * initialOffsets, mapOffsetY: -1 * initialOffsets};
-const plotView = {plotId: "", plotX: 0, plotY: 0, locationX: 0, locationY: 0};
+const mapView = { mapOffsetX: -1 * initialOffsets, mapOffsetY: -1 * initialOffsets };
+const plotView = { plotId: "", plotX: 0, plotY: 0, locationX: 0, locationY: 0 };
 
 // canvas drawing functions
-function drawCanvas() { 
+function drawCanvas() {
     mainCanvas.width = 3 * plots + 4 * roads;
     mainCanvas.height = 3 * plots + 4 * roads;
     plotCanvas.width = plots;
@@ -36,17 +36,68 @@ function initalizeMap() {
     drawMapSection(plotCtx, -1 * plotView.locationX, -1 * plotView.locationY);
 }
 
+//animate functions
+function move(direction) {
+    const validMove = validateMove(direction);
+    if (validMove) {
+        updateView(direction);
+        drawMapSection(mainCtx, mapView.mapOffsetX, mapView.mapOffsetY);
+        updatePlotLocation();
+        drawCursor(plotViewOffsets, plotViewOffsets);
+        drawMapSection(plotCtx, -1 * plotView.locationX, -1 * plotView.locationY);
+    } else {
+        console.log("invalid move");
+    }
+}
+
+function validateMove(direction) {
+    switch (direction) {
+        case "ArrowRight": return !(plotView.plotX == 5);
+        case "ArrowUp": return !(plotView.plotX == 0);
+        case "ArrowLeft": return !(plotView.plotY == 0);
+        case "ArrowDown": return !(plotView.plotY == 5);
+    }
+}
+
+function updateView(direction) {
+    switch (direction) {
+        case "ArrowRight":
+            plotView.plotX += 1;;
+            mapView.mapOffsetX -= plots + roads;
+            updatePlotLocation();
+            break;
+        case "ArrowDown":
+            plotView.plotY += 1;
+            mapView.mapOffsetY -= plots + roads;
+            updatePlotLocation();
+            break;
+        case "ArrowLeft":
+            plotView.plotX -= 1;
+            mapView.mapOffsetX += plots + roads;
+            updatePlotLocation();
+            break;
+        case "ArrowUp":
+            plotView.plotY -= 1;
+            mapView.mapOffsetY += plots + roads;
+            updatePlotLocation();
+            break;
+    }
+}
+
 function drawMapSection(ctx, originX, originY) {
     ctx.drawImage(worldImage, originX, originY);
 }
 
-function drawCursor (x, y) {
+function drawCursor(x, y) {
     mainCtx.strokeRect(x, y, plots, plots);
 }
 
-function updatePlotLocation(){
+function updatePlotLocation() {
     plotView.locationX = -1 * mapView.mapOffsetX + plotViewOffsets;
     plotView.locationY = -1 * mapView.mapOffsetY + plotViewOffsets;
 }
 
 drawCanvas();
+window.addEventListener("keydown", (event) => {
+    move(event.key);
+});
