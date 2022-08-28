738d55ea51b811130b4b285898fe811ce91e122b
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-08-28 11:04:59 -0400

    Show map section

--- Moraland_Frontend/static/js/logic.js
+++ Moraland_Frontend/static/js/logic.js
@@ -1,27 +1,39 @@
 //Graphics constants
 // - map management constants
-const tiles = 12; //in pixels
-const plots = tiles * 9; //in pixels
-const roads = tiles * 2; //in pixels
-// const initialOffsets = 1 * (plots + roads);
-// const plotViewOffsets = 1 * (plots + (2 * roads));
+const tiles = 12;
+const plots = tiles * 9;
+const roads = tiles * 2;
+const initialOffsets = plots + roads;
+const plotViewOffsets = plots + (2 * roads);
 
 // - canvas and context
 const mainCanvas = document.getElementById("mainCanvas");
-//Add a p tag to the page with the given text
-var mainCtx = mainCanvas.getContext("2d");
-
+const mainCtx = mainCanvas.getContext("2d");
+const plotCanvas = document.getElementById("plotCanvas");
+const plotCtx = plotCanvas.getContext("2d");
 const worldImage = new Image();
 
+// - State
+const mapView = {mapOffsetX: -1 * initialOffsets, mapOffsetY: -1 * initialOffsets};
+const plotView = {plotId: "", plotX: 0, plotY: 0, locationX: 0, locationY: 0};
+
 // canvas drawing functions
-function drawCanvas() {
+function drawCanvas() { 
     mainCanvas.width = 3 * plots + 4 * roads;
     mainCanvas.height = 3 * plots + 4 * roads;
+    plotCanvas.width = plots;
+    plotCanvas.height = plots;
     worldImage.src = 'static/img/Moraland.png';
     worldImage.onload = () => {
-        mainCtx.drawImage(worldImage, -1 * (plots + roads), -1 * (plots + roads));
-        mainCtx.strokeRect(plots + (2 * roads), plots + (2 * roads), plots, plots);
+        initalizeMap();
     }
+}
+
+function initalizeMap() {
+    updatePlotLocation();
+    drawMapSection(mainCtx, mapView.mapOffsetX, mapView.mapOffsetY);
+    drawCursor(plotViewOffsets, plotViewOffsets);
+    drawMapSection(plotCtx, -1 * plotView.locationX, -1 * plotView.locationY);
 }
 
 function drawMapSection(ctx, originX, originY) {
@@ -30,6 +42,11 @@ function drawMapSection(ctx, originX, originY) {
 
 function drawCursor (x, y) {
     mainCtx.strokeRect(x, y, plots, plots);
+}
+
+function updatePlotLocation(){
+    plotView.locationX = -1 * mapView.mapOffsetX + plotViewOffsets;
+    plotView.locationY = -1 * mapView.mapOffsetY + plotViewOffsets;
 }
 
 drawCanvas();
